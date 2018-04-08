c81a48af15835a258809d9d415a4b25c55432844
chaymankala <chaithanya.mankala@gmail.com>
2018-04-08 17:36:50 +0530

    removed comments and a bug fix

--- extension.js
+++ extension.js
@@ -1,27 +1,15 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 const vscode = require('vscode');
 
 
 var _interval = -1;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
 
     var extensionConfig = vscode.workspace.getConfiguration('ThemeSwitch');
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log(userSettings.workbench.colorTheme);
-    console.log(vscode)
-
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('ThemeSwitch.sayDude', function () {
-        // The code you place here will be executed every time your command is executed
+
         var userSettings = vscode.workspace.getConfiguration();
         if (_interval == -1) {
             let themeList = extensionConfig.themeList;
@@ -32,7 +20,7 @@ function activate(context) {
                 ]
             }
             if (themeList.length) {
-                // Display a message box to the user
+
                 vscode.window.showInformationMessage('Started Switching!');
                 var i = 0;
 
@@ -48,17 +36,9 @@ function activate(context) {
 
     });
     vscode.commands.registerCommand('ThemeSwitch.stopDude', function () {
-        // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
         vscode.window.showInformationMessage('Stopped switching!');
-        try {
-            clearInterval(_interval);
-            _interval = -1;
-        }
-        catch (err) {
-            console.log(err);
-        }
+        stopSwitch();
 
 
     });
@@ -68,7 +48,19 @@ function activate(context) {
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
+function stopSwitch() {
+    try {
+        clearInterval(_interval);
+        _interval = -1;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+
+
 function deactivate() {
+    stopSwitch();
 }
 exports.deactivate = deactivate;
